ff979b21ff0483febf9de5dc55c6d25099f9dbf5
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-12-23 11:09:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread chickenThread = new Thread(PrintChicken);
+            Thread eggThread = new Thread(PrintEgg);
+            Console.WriteLine("Спор начался! Участники спора: ");
+            chickenThread.Start();
+            eggThread.Start();
+
+
+            chickenThread.Join();
+            eggThread.Join();
+
+
+            if (eggThread.IsAlive)
+            {
+                Console.WriteLine("Побеждает: Яйцо");
+            }
+            else
+            {
+                Console.WriteLine("Побеждает: Курица");
+            }
+        }
+        static void PrintEgg()
+        {
+            Console.WriteLine("Яйцо");
+        }
+        static void PrintChicken()
+        {
+            Console.WriteLine("Курица");
+        }
+    }
+    }
+
